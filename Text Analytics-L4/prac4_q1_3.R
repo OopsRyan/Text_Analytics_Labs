69044809773dc23cc0816db5249cfb73b108cb9a
Qirun Chen <qirun_chen@163.com>
2017-10-11 19:28:51 +0100

    Delete prac4_q1_3.R

--- Text Analytics-L4/prac4_q1_3.R
+++
@@ -1,4 +0,0 @@
-library(wordcloud)
-library(tm)
-wordcloud("data attributes What attributes, get This look values attribute Can learn Section These value)",
-min.freq=1, colors=brewer.pal(6,"Dark2"), random.order=FALSE)
